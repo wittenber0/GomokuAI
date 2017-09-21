c5a6f8a3579a76fe6837b26159eb755162791cb9
wittenber0 <ryanwwittenberg@gmail.com>
2017-09-20 21:53:28 -0400

    added classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Created by Ryan on 9/20/2017.
  */
@@ -5,7 +8,19 @@ public class Main {
 
     public static void main(String[] args){
 
-        
+
+
+    }
+
+    public void writeMove(Move m){
+
+        try {
+            Scanner scanner = new Scanner(new File("move_file_test"), " ");
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
 
     }
 }
