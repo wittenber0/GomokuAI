4ab2f00f1ca8a241eb0d9c0e43411f4a5a88232f
Luke Buquicchio <ljbuquicchio@wpi.edu>
2017-09-20 20:51:40 -0400

    Setter and Getter and Initializer

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -3,6 +3,21 @@
  */
 public class GameBoard {
     public enum TileType {black, white, empty}
-
     public TileType[][] board = new TileType[15][15];
+
+    public GameBoard() {
+        for(int i=0; i<15; i++) {
+            for(int j=0; j<15; j++) {
+                this.board[i][j] = TileType.empty;
+            }
+        }
+    }
+
+    public TileType[][] getBoard() {
+        return this.board;
+    }
+
+    public void setBoard(int i, int j, TileType type) {
+        this.board[i][j] = type;
+    }
 }
